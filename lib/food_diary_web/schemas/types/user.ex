f28cbadd5e0d03524ca9fbf7e5bc358c947a8070
joabehenrique <henriquecidoz@hotmail.com>
2022-10-12 16:22:42 -0300

    feat: created user types

---
+++ lib/food_diary_web/schemas/types/user.ex
@@ -0,0 +1,15 @@
+defmodule FoodDiaryWeb.Schemas.Types.User do
+  use Absinthe.Schema.Notation
+
+  @desc "Types user auth"
+  object :user do
+    field :id, non_null(:id), description: "is a integer"
+    field :name, non_null(:string)
+    field :email, non_null(:string)
+  end
+
+  input_object :input_create_user do
+    field :name, non_null(:string), description: "Users name"
+    field :email, non_null(:string), description: "Users email"
+  end
+end
