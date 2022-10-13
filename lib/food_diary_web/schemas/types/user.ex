b714f76e5afaf9bd6bc2759f37aacbbaaad5e8af
joabehenrique <henriquecidoz@hotmail.com>
2022-10-12 23:37:16 -0300

    feat: linked to the meal list

--- lib/food_diary_web/schemas/types/user.ex
+++ lib/food_diary_web/schemas/types/user.ex
@@ -6,6 +6,7 @@ defmodule FoodDiaryWeb.Schemas.Types.User do
     field :id, non_null(:id), description: "is a integer"
     field :name, non_null(:string)
     field :email, non_null(:string)
+    field :meals, list_of(:meal)
   end
 
   input_object :input_create_user do
