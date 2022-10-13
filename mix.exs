180879b805c5d6933f7cac6fd5a6125602355ec9
joabehenrique <henriquecidoz@hotmail.com>
2022-10-12 23:30:42 -0300

    deps: added absinthe_phoenix

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule FoodDiary.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:absinthe, "~> 1.6"},
       {:absinthe_plug, "~> 1.5"},
-      {:crudry, "~> 2.4.0"}
+      {:crudry, "~> 2.4.0"},
+      {:absinthe_phoenix, "~> 2.0"}
     ]
   end
 
