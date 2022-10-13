58980ec89eb40bdca1db51a6fed4069794b34798
joabehenrique <henriquecidoz@hotmail.com>
2022-10-12 23:33:43 -0300

    feat: created meal type

---
+++ lib/food_diary_web/schemas/types/meal.ex
@@ -0,0 +1,24 @@
+defmodule FoodDiaryWeb.Schemas.Types.Meal do
+  use Absinthe.Schema.Notation
+
+  enum :category do
+    value :food
+    value :drink
+    value :pizza
+  end
+
+  @desc "Types meal auth"
+  object :meal do
+    field :id, non_null(:id)
+    field :description, non_null(:string)
+    field :calories, non_null(:float)
+    field :category, non_null(:category)
+  end
+
+  input_object :create_meal_input do
+    field :user_id, non_null(:id)
+    field :description, non_null(:string)
+    field :calories, non_null(:float)
+    field :category, non_null(:category)
+  end
+end
