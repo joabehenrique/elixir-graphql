819597d8278e56f22d1c612b6e2e42546e3f58bd
joabehenrique <henriquecidoz@hotmail.com>
2022-10-12 23:36:15 -0300

    feat: added meals querys

--- lib/food_diary_web/schemas/types/root.ex
+++ lib/food_diary_web/schemas/types/root.ex
@@ -2,7 +2,10 @@ defmodule FoodDiaryWeb.Schemas.Types.Root do
   use Absinthe.Schema.Notation
 
   alias FoodDiaryWeb.Resolvers.User, as: UsersResolver
+  alias FoodDiaryWeb.Resolvers.Meal, as: MealsResolver
+  alias Crudry.Middlewares.TranslateErrors
 
+  import_types FoodDiaryWeb.Schemas.Types.Meal
   import_types FoodDiaryWeb.Schemas.Types.User
 
   object :root_query do
@@ -19,6 +22,7 @@ defmodule FoodDiaryWeb.Schemas.Types.Root do
       arg :input, non_null(:input_create_user)
 
       resolve &UsersResolver.create/2
+      middleware TranslateErrors
     end
 
     @desc "Delete User"
@@ -26,6 +30,32 @@ defmodule FoodDiaryWeb.Schemas.Types.Root do
       arg :id, non_null(:id)
 
       resolve &UsersResolver.delete/2
+      middleware TranslateErrors
+    end
+
+    @desc "Create Meal"
+    field :create_meal, :meal do
+      arg :input, non_null(:create_meal_input)
+
+      resolve &MealsResolver.create/2
+      middleware TranslateErrors
+    end
+  end
+
+  object :root_subscription do
+    # field :new_meal, :meal do
+    #   config fn _args, _info ->
+    #     {:ok, topic: "new_meal_topic"}
+    #   end
+    # end
+
+    #Com triggers
+    field :new_meal, :meal do
+      config fn _args, _info ->
+        {:ok, topic: "new_meal_topic"}
+      end
+
+      trigger :create_meal, topic: fn _context -> ["new_meal_topic"] end
     end
   end
 end
